56d809bd4b9462ff13bddb767404f1dabf604710
mike239x <mike239x@gmail.com>
2018-03-17 18:43:38 +0100

    Removed main index.html
    Main purpose of that file was to hide .git dir and probably make better
    links to projects. This cannot done by HTML only so I gonna ignore that
    for now.

---
+++ TODO.md
@@ -0,0 +1,2 @@
+1. port all my applet creations to javascript.
+1050. create an `index.html` file in main directory listing all projects in some fancy way. AFAIK this can either be done by some static typing (not my way to go) or using some PHP (which I do not know as of yet).
--- basic-example/sketch.js
+++ basic-example/sketch.js
@@ -3,10 +3,11 @@ function setup() {
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
+  // if (mouseIsPressed) {
+  //   fill(0);
+  // } else {
+  //   fill(255);
+  // }
+  fill(250,250,250,10)
   ellipse(mouseX, mouseY, 80, 80);
 }
