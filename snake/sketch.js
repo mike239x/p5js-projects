b08c0616bea4a175d8b1220d5f1e6a6387b1e5f0
mike239x <mike239x@gmail.com>
2018-03-18 12:25:06 +0100

    First stable release

--- snake/sketch.js
+++ snake/sketch.js
@@ -22,7 +22,7 @@ var head = {
 var myFont;
 
 function preload() {
-    myFont = loadFont('../fonts/handwritten/discover-earth/font.ttf');
+    myFont = loadFont('../fonts/handwritten/love-and-trust/LoveAndTrust.ttf');
 }
 
 function restartGame() {
@@ -39,19 +39,20 @@ function restartGame() {
 
 function setup() {
     createCanvas(WIDTH,HEIGHT);
-    frameRate(10);
-    boxSize = 8;
-    fieldWigth = floor(WIDTH / boxSize), fieldHeight = floor(HEIGHT / boxSize);
+    frameRate(30);
+    boxSize = 7;
+    fieldWigth = 50, fieldHeight = 50;
     field = [];
     for (let x = 0; x < fieldWigth; x++) {
         field.push([]);
         for (let y = 0; y < fieldHeight; y++)
             field[x].push(0);
     }
-    appleSize = 2;
-    dropRate = 0.1;
+    appleSize = 5;
+    dropRate = 1;
     gameState = -1; // starting screen
     background(250,0,0);
+    textFont(myFont, 32);
 }
 
 function drawTile(type) {
@@ -61,35 +62,40 @@ function drawTile(type) {
     if (type > 0) {
         var a = map(type, 0, len, 0, 256);
         // strokeWeight(1);
-        stroke(COLOR.YELLOW, a);
-        fill(COLOR.BLACK);
-        ellipse(0,0,boxSize,boxSize);
+        stroke(COLOR.YELLOW.concat([a]));
+        fill(COLOR.BLACK.concat([a]));
+        ellipse(boxSize/2,boxSize/2,boxSize);
     } else if (type < 0) {
         stroke(COLOR.RED);
-        push();
-        translate(boxSize/2 - appleSize, boxSize/2 - appleSize);
-        ellipse(0,0,2*appleSize,2*appleSize);
-        pop();
+        ellipse(boxSize/2,boxSize/2,appleSize);
     }
 }
 
 function draw() {
     switch (gameState) {
         case -1: // game just started
-            push();
-            textFont(myFont);
-            translate(WIDTH/2, HEIGHT/2);
             background(COLOR.DARKGRAY);
+            push();
+            translate(WIDTH/2, HEIGHT/2);
             stroke(COLOR.ORANGE);
-            textSize(32);
+            fill(COLOR.ORANGE);
             textAlign(CENTER);
+            // textLeading(0);
             text('This is welcome screen.\n\
-            Use WASD to move around.\n\
-            Press ENTER to start.',0,0);
+                Use WASD to move around.\n\
+                Press ENTER to start.',0,-16);
             pop();
             break;
         case 1: // end of the game screen
-            //TODO draw endgame screen
+            background(COLOR.DARKGRAY);
+            push();
+            translate(WIDTH/2, HEIGHT/2);
+            stroke(COLOR.ORANGE);
+            fill(COLOR.ORANGE);
+            textAlign(CENTER);
+            text(`You ate ${(len-1)} cyber-apples. Well done!\n\
+                Press ENTER to restart.`,0,-16);
+            pop();
             break;
         default: // playing
             var newDir;
@@ -118,6 +124,8 @@ function draw() {
             if (0 > head.x || head.x >= fieldWigth ||
                 0 > head.y || head.y >= fieldHeight) {
                 gameState = 1;
+            } else if (field[head.x][head.y] > 1) {
+                gameState = 1;
             } else {
                 if (field[head.x][head.y] == -1) {
                     len++;
@@ -131,7 +139,13 @@ function draw() {
                     }
                 }
                 field[head.x][head.y] = len;
-                //TODO add apples
+                if (random(1) < dropRate) {
+                        var x = floor(random(fieldWigth));
+                        var y = floor(random(fieldHeight));
+                        if (field[x][y] == 0) {
+                            field[x][y] = -1;
+                        }
+                }
             }
             for (let x = 0; x < fieldWigth; x++) {
                 for (let y = 0; y < fieldHeight; y++) {
