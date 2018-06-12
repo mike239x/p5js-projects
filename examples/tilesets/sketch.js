c4486f9c1fcec944aa9e756cdbcbdef57663cd56
mike239x <mikelezhnin@gmail.com>
2018-06-12 09:43:10 +0200

    finished tilemap.js

--- examples/tilesets/sketch.js
+++ examples/tilesets/sketch.js
@@ -1,8 +1,5 @@
 let WIDTH = 640, HEIGHT = 448;
-let mytiledmap;
 let mymap;
-let _ = undefined;
-let rules;
 
 let debug = false;
 let pencil;
@@ -14,120 +11,12 @@ function preload() {
 let canvas;
 
 function setup() {
-    mytiledmap = new Tilemap(pencil, 64, 10, 7);
 
-    mymap = new Map(10,7);
-    // mymap.load([
-    //   [0,1,1,0,0,0,0,0,1,1],
-    //   [1,1,1,0,0,0,0,1,1,1],
-    //   [0,0,0,1,1,0,0,1,0,0],
-    //   [0,0,1,1,1,0,0,0,0,0],
-    //   [1,1,0,0,1,0,0,1,1,0],
-    //   [1,1,1,0,0,0,1,1,1,0],
-    //   [0,0,0,0,0,0,1,0,1,0]
-    // ]);
+    mymap = new Layer(pencil,64,10,7);
+    mymap.outside = 0;
     mymap.random();
-
-    rules = [
-      { pattern : [[1,1,1],[1,1,1],[1,1,1]],
-        tile : 1 },
-      { pattern : [[_,0,_],[0,1,1],[_,1,1]],
-        tile : 16 },
-      { pattern : [[_,0,_],[1,1,0],[1,1,_]],
-        tile : 17 },
-      { pattern : [[_,1,1],[0,1,1],[_,0,_]],
-        tile : 32 },
-      { pattern : [[1,1,_],[1,1,0],[_,0,_]],
-        tile : 33 },
-      { pattern : [[1,1,_],[1,1,0],[1,1,_]],
-        tile : 48 },
-      { pattern : [[_,0,_],[1,1,1],[1,1,1]],
-        tile : 49 },
-      { pattern : [[_,1,1],[0,1,1],[_,1,1]],
-        tile : 64 },
-      { pattern : [[1,1,1],[1,1,1],[_,0,_]],
-        tile : 65 },
-      { pattern : [[1,1,1],[1,1,1],[1,1,0]],
-        tile : 80 },
-      { pattern : [[1,1,1],[1,1,1],[0,1,1]],
-        tile : 81 },
-      { pattern : [[1,1,0],[1,1,1],[1,1,1]],
-        tile : 96 },
-      { pattern : [[0,1,1],[1,1,1],[1,1,1]],
-        tile : 97 },
-      { pattern : [[_,0,_],[1,1,1],[_,0,_]],
-        tile : 112 },
-      { pattern : [[_,1,_],[0,1,0],[_,1,_]],
-        tile : 113 },
-      { pattern : [[_,0,_],[0,1,1],[_,0,_]],
-        tile : 128 },
-      { pattern : [[_,1,_],[0,1,0],[_,0,_]],
-        tile : 129 },
-      { pattern : [[_,0,_],[0,1,0],[_,1,_]],
-        tile : 144 },
-      { pattern : [[_,0,_],[1,1,0],[_,0,_]],
-        tile : 145 },
-      { pattern : [[0,1,1],[1,1,1],[0,1,1]],
-        tile : 160 },
-      { pattern : [[1,1,1],[1,1,1],[0,1,0]],
-        tile : 161 },
-      { pattern : [[0,1,0],[1,1,1],[1,1,1]],
-        tile : 176 },
-      { pattern : [[1,1,0],[1,1,1],[1,1,0]],
-        tile : 177 },
-      { pattern : [[0,1,1],[1,1,1],[1,1,0]],
-        tile : 192 },
-      { pattern : [[1,1,0],[1,1,1],[0,1,1]],
-        tile : 193 },
-      { pattern : [[_,0,_],[0,1,0],[_,0,_]],
-        tile : 208 },
-      { pattern : [[0,1,0],[1,1,1],[0,1,0]],
-        tile : 209 },
-      { pattern : [[_,0,_],[0,1,1],[_,1,0]],
-        tile : 224 },
-      { pattern : [[_,0,_],[1,1,0],[0,1,_]],
-        tile : 225 },
-      { pattern : [[_,1,0],[0,1,1],[_,0,_]],
-        tile : 240 },
-      { pattern : [[0,1,_],[1,1,0],[_,0,_]],
-        tile : 241 },
-      { pattern : [[1,1,0],[1,1,1],[_,0,_]],
-        tile : 26 },
-      { pattern : [[0,1,1],[1,1,1],[_,0,_]],
-        tile : 27 },
-      { pattern : [[1,1,_],[1,1,0],[0,1,_]],
-        tile : 28 },
-      { pattern : [[_,1,1],[0,1,1],[_,1,0]],
-        tile : 29 },
-      { pattern : [[0,1,_],[1,1,0],[0,1,_]],
-        tile : 30 },
-      { pattern : [[_,0,_],[1,1,1],[0,1,0]],
-        tile : 31 },
-      { pattern : [[_,0,_],[1,1,1],[1,1,0]],
-        tile : 42 },
-      { pattern : [[_,0,_],[1,1,1],[0,1,1]],
-        tile : 43 },
-      { pattern : [[0,1,_],[1,1,0],[1,1,_]],
-        tile : 44 },
-      { pattern : [[_,1,0],[0,1,1],[_,1,1]],
-        tile : 45 },
-      { pattern : [[_,1,0],[0,1,1],[_,1,0]],
-        tile : 46 },
-      { pattern : [[0,1,0],[1,1,1],[_,0,_]],
-        tile : 47 },
-      { pattern : [[0,1,0],[1,1,1],[0,1,1]],
-        tile : 186 },
-      { pattern : [[0,1,1],[1,1,1],[0,1,0]],
-        tile : 187 },
-      { pattern : [[1,1,0],[1,1,1],[0,1,0]],
-        tile : 188 },
-      { pattern : [[0,1,0],[1,1,1],[1,1,0]],
-        tile : 189 },
-      { pattern : [[_,_,_],[_,0,_],[_,_,_]],
-        tile : 0 }
-    ];
-
-    mytiledmap.generateFrom(rules,mymap);
+    mymap.rules = BASIC_PENCIL;
+    mymap.compile();
 
     canvas = createCanvas(WIDTH,HEIGHT);
 
@@ -135,7 +24,7 @@ function setup() {
       let i = floor(map(mouseX, 0, WIDTH, 0, 10));
       let j = floor(map(mouseY, 0, HEIGHT, 0, 7));
       mymap.set(1-mymap.get(i,j),i,j);
-      mytiledmap.generateFrom(rules,mymap);
+      // mytiledmap.generateFrom(rules,mymap);
     });
 
     let debugCheckbox = createCheckbox('debug mode', false);
@@ -145,25 +34,25 @@ function setup() {
 
 }
 
-function loadTileset() {
-  mytiledmap.load([
-    [  0,  1, 16, 17, 32, 33, 48, 49, 64, 65],
-    [ 80, 81, 96, 97,112,113,128,129,144,145],
-    [160,161,176,177,192,193,208,209,224,225],
-    [240,241, 26, 27, 28, 29, 30, 31, 42, 43],
-    [44,45,46,47,186,187,188,189,0,0],
-    [0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0]
-  ]);
-}
+// function loadTileset() {
+//   mytiledmap.load([
+//     [  0,  1, 16, 17, 32, 33, 48, 49, 64, 65],
+//     [ 80, 81, 96, 97,112,113,128,129,144,145],
+//     [160,161,176,177,192,193,208,209,224,225],
+//     [240,241, 26, 27, 28, 29, 30, 31, 42, 43],
+//     [44,45,46,47,186,187,188,189,0,0],
+//     [0,0,0,0,0,0,0,0,0,0],
+//     [0,0,0,0,0,0,0,0,0,0]
+//   ]);
+// }
 
 function draw() {
   background(255,0,0);
-  mytiledmap.draw(0,0);
+  mymap.draw(0,0);
   if (debug) {
     for ( let i = 0; i < 10; i++) {
       for ( let j = 0; j < 7; j++) {
-        text(mytiledmap.map[i][j],i*64+10,j*64+40);
+        text(mymap.tiles.data[i][j],i*64+10,j*64+40);
       }
     }
   }
--- examples/tilesets/tilemap.js
+++ examples/tilesets/tilemap.js
@@ -1,141 +1,33 @@
 // TODO: mb later make it a proper module
-class Tilemap {
-  // this requires a p5.js framework
-  constructor(tileset, tilesize, width, height) {
-    this.ts = tilesize;
-    this.img = createGraphics(tilesize*width,tilesize*height);
-    //TODO: find out if graphics or image is better
-    this.map = [];
-    this.width = width;
-    this.height = height;
-    for (let i = 0; i < width; i++) {
-      this.map.push((new Array(height)).fill(0));
-    }
-    this.center = {
-      x : -0.5,
-      y : -0.5
-    };
-    this.tileset = tileset;
-    this.tileset.loadPixels();
-    let w = round(this.tileset.width / tilesize);
-    let h = round(this.tileset.height / tilesize);
-    this.indexOf = {};
-    let k = 0;
-    //index all the tiles, from top to bottom left to right
-    for (let j = 0; j < h; j++) {
-      for (let i = 0; i < w; i++) {
-        this.indexOf[k] = {
-          x : i,
-          y : j
-        }
-        k++;
-      }
-    };
-  }
-  // load the map from the given 2D array
-  load(a) {
-    let i,j;
-    j = 0;
-    for (let line of a) {
-      i = 0;
-      for (let c of line) {
-        this.set(c,i,j);
-        i++;
-      }
-      j++;
-    }
-  }
-  // set the tile at (x,y) to t
-  // `soft` setting means if you try to put the same tile where it was, you do nothing
-  set(t, x,y, soft = true) {
-    if (soft && this.map[x][y] == t) return;
-    this.map[x][y] = t;
-    let i = this.indexOf[t].x;
-    let j = this.indexOf[t].y;
-    let s = this.ts;
-    this.img.loadPixels();
-    for (let a = 0; a < s; a++) {
-      for (let b = 0; b < s; b++) {
-        for (let k = 0 ; k < 4; k++) {
-          this.img.pixels[4*((y*s+a)*this.img.width + x*s + b) + k] =
-            this.tileset.pixels[4*((j*s+a)*this.tileset.width + i*s + b) + k];
-        }
-      }
-    }
-    this.img.updatePixels();
-  }
-  // draw the tilemap
-  draw(x,y) {
-    image(this.img, x - (this.center.x+0.5) * this.ts, y - (this.center.y+0.5) * this.ts);
-  }
 
-  center(i,j) {
-    this.center = {
-      x : i,
-      y : j
-    };
-  }
+//TODO: transform comments above methods into doc-strings
 
-  //TODO: mb rename this
-  // takes patterns and the map to generate tiles
-  // all the patterns look like this:
-  // {
-  //   pattern : <obj>,
-  //   tile : "tilename"
-  // }
-  // for each tile the patterns are looked left to right and after
-  // the first matching pattern is met the tile is changed and it goes further
-  // if no patterns are matched tile is unchanged
-  // P.S. uses soft setting
-  generateFrom(rules, map) {
-    for (let i = 0; i < this.width; i++) {
-      for (let j = 0; j < this.height; j++) {
-        for (let x of rules) {
-          if (map.matches(x.pattern, i,j)) {
-            this.set(x.tile,i,j);
-            break;
-          }
-        }
-      }
-    }
-  }
-}
+const _ = undefined;
 
-// a wrap for 2d array
+// soft wrap of a 2D array
+class Array2D {
 
-class Map {
-
-  constructor(w,h) {
+  constructor (w, h) {
     this.width = w;
     this.height = h;
     this.data = [];
     for (let i = 0; i < w; i++) {
       this.data.push((new Array(h)).fill(0));
     }
-    this.outside = 0;
+    this.outside = undefined;
   }
 
-  load(a) {
-    let i,j;
-    j = 0;
+  load (a) {
+    let i = 0, j = 0;
     for (let line of a) {
       i = 0;
       for (let c of line) {
-        this.set(c,i,j);
+        this.data[i][j] = c;
         i++;
       }
       j++;
     }
   }
-
-  random() {
-    for (let i = 0; i < this.width; i++) {
-      for (let j = 0; j < this.height; j++) {
-        this.data[i][j] = round(random(0,1));
-      }
-    }
-  }
-
 
   fill (x) {
     for (let i = 0; i < this.width; i++) {
@@ -145,13 +37,25 @@ class Map {
     }
   }
 
-  set (x, i, j) {
-    if (i >= 0 && j >= 0 && i < this.width && j < this.height) {
-      this.data[i][j] = x;
-      return 1;
+  random () {
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        this.data[i][j] = round(random(0,1));
+      }
     }
   }
 
+  // tries to set data[i][j] to c, returns 1 if anything changed
+  set (c, i, j) {
+    if (i >= 0 && j >= 0 && i < this.width && j < this.height) {
+      if (this.data[i][j] == c) return 0;
+      this.data[i][j] = c;
+      return 1;
+    }
+    return undefined;
+  }
+
+  // tries to retrieve data[i][j], if out of bounds returns default value
   get (i,j) {
     if (i >= 0 && j >= 0 && i < this.width && j < this.height) {
       return this.data[i][j];
@@ -174,4 +78,206 @@ class Map {
     }
     return true;
   }
+
+  filter (f) {
+    if (f == undefined) {
+      f = x => x; // can also be used for copying
+    }
+    let re = new Map(this.width, this.height);
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        re.data[i][j] = f(this.data[i][j]);
+      }
+    }
+    return re;
+  }
+
+  [Symbol.iterator] () {
+    return this.data[Symbol.iterator]();
+  }
+  // or also:
+  // *[Symbol.iterator] () {
+  //   for (let c of this.data) yield c;
+  // }
+
+}
+
+class Tiles extends Array2D {
+
+  // this requires a p5.js framework
+  constructor(tileset, tilesize, width, height) {
+    super(width, height);
+    this.ts = tilesize;
+    this.img = createGraphics(tilesize*width,tilesize*height);
+    //TODO: find out if graphics or image is better
+    this.tileset = tileset;
+    this.tileset.loadPixels();
+    let w = round(this.tileset.width / tilesize);
+    let h = round(this.tileset.height / tilesize);
+    this.indexOf = {};
+    let k = 0;
+    //index all the tiles, from top to bottom left to right
+    for (let j = 0; j < h; j++) {
+      for (let i = 0; i < w; i++) {
+        this.indexOf[k] = {
+          x : i,
+          y : j
+        }
+        k++;
+      }
+    };
+  }
+
+  // set the tile at (x,y) to t
+  // `soft` setting means if you try to put the same tile where it was, you do nothing
+  // `hard` setting may be useful if you changed the tile names and
+  // want to set different tile which has the same name as the last one
+  set(t, x, y, soft = true) {
+    let q = super.set(t,x,y);
+    if (q == undefined) return undefined;
+    if (soft && q == 0) return 0;
+    let i = this.indexOf[t].x;
+    let j = this.indexOf[t].y;
+    let s = this.ts;
+    this.img.loadPixels();
+    for (let a = 0; a < s; a++) {
+      for (let b = 0; b < s; b++) {
+        for (let k = 0 ; k < 4; k++) {
+          this.img.pixels[4*((y*s+a)*this.img.width + x*s + b) + k] =
+            this.tileset.pixels[4*((j*s+a)*this.tileset.width + i*s + b) + k];
+        }
+      }
+    }
+    this.img.updatePixels();
+    return 1;
+  }
+
+  compile () {
+    for (let x = 0; x < this.width; x++) {
+      for (let y = 0; y < this.height; y++) {
+        let t = this.data[x][y];
+        let i = this.indexOf[t].x;
+        let j = this.indexOf[t].y;
+        let s = this.ts;
+        this.img.loadPixels();
+        for (let a = 0; a < s; a++) {
+          for (let b = 0; b < s; b++) {
+            for (let k = 0 ; k < 4; k++) {
+              this.img.pixels[4*((y*s+a)*this.img.width + x*s + b) + k] =
+                this.tileset.pixels[4*((j*s+a)*this.tileset.width + i*s + b) + k];
+            }
+          }
+        }
+        this.img.updatePixels();
+      }
+    }
+  }
+
+  // draw the tilemap
+  // x and y be the screen position
+  // cx and cy be the cell to focus on
+  // scale is the scale (greater than zero)
+  draw (x, y, cx = -0.5, cy = -0.5, scale = 1) {
+    image(this.img,
+      x - (cx+0.5) * this.ts * scale,
+      y - (cy+0.5) * this.ts * scale,
+      this.img.width * scale,
+      this.img.height * scale
+    );
+  }
+
+}
+
+// a wrap for 2d array with a tileset for displaying
+// and a set of rules for the tilemap generation
+class Layer extends Array2D {
+
+  constructor (tileset, tilesize, width, height) {
+    super(width,height);
+    this.tiles = new Tiles(tileset, tilesize, width, height);
+    this.rules = [
+      { pattern : [[_,_,_],[_,1,_],[_,_,_]],
+        tile : 1 },
+      { pattern : [[_,_,_],[_,0,_],[_,_,_]],
+        tile : 0 }
+    ];
+  }
+
+  compile () {
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        for (let r of this.rules) {
+          if (this.matches(r.pattern, i, j)) {
+            this.tiles.set(r.tile, i, j);
+            break;
+          }
+        }
+      }
+    }
+  }
+
+  // recompile means immediately apply changes to the tiles
+  // use recompile = false to postpone the changes to tiles
+  // so you can use compile later on
+  set (c, i, j, recompile = true) {
+    let q = super.set(c,i,j);
+    if (q == undefined) return undefined;
+    if (q == 0) return 0;
+    if (recompile) {
+      for (let x = 0; x < 3; x++) {
+        for (let y = 0; y < 3; y++) {
+          if (i+x-1 >= 0 && j+y-1 >= 0 && i+x-1 < this.width && j+y-1 < this.height) {
+            for (let r of this.rules) {
+              if (this.matches(r.pattern, i+x-1,j+y-1)) {
+                this.tiles.set(r.tile,i+x-1,j+y-1);
+                break;
+              }
+            }
+          }
+        }
+      }
+    }
+    return 1;
+  }
+
+  draw (x, y, cx = -0.5, cy = -0.5, scale = 1) {
+    this.tiles.draw(x, y, cx, cy, scale);
+  }
+
+}
+
+class Map extends Array2D {
+
+  // here tilesize is the expected tilesize
+  // (in case different layers have different ts)
+  constructor (tilesize, w,h) {
+    super(w,h);
+    this.ts = tilesize;
+    this.layers = [];
+    // constructor (tileset, tilesize, width, height) {
+  }
+
+  compile() {
+    for (let layer of this.layers) {
+      layer.load(this.filter(layer.filter));
+      layer.compile();
+    }
+  }
+
+  set (c, i, j) {
+    let q = super.set(c,i,j);
+    if (q == undefined) return undefined;
+    if (q == 0) return 0;
+    for (let layer of this.layers) {
+      layer.set(layer.filter(c),i,j);
+    }
+  }
+
+  //TODO: think if the scale is needed
+  draw (x, y, cx, cy, scale) {
+    for (let layer of this.layers) {
+      layer.draw(x,y,cx,cy,scale*this.ts/layer.tiles.ts);
+    }
+  }
+
 }
